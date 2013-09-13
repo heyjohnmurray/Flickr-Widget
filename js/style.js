d163596d5b158a2c4afd7a31f057a0c862318810
John <jmurray@redventures.com>
2013-09-13 01:33:14 -0400

    widget size creator done!
    i did it!!! see comments for notes on how i did it. basically, use
    .filter() to refrain from putting "highlight" class on any li that was
    on a higher row than the <li> i clicked on. then further filter the
    <li>s that have the "highlight" class by using "removeClass" from any
    <li> in a column further to the right than the <li> i clicked on.

--- js/style.js
+++ js/style.js
@@ -15,25 +15,31 @@ $(function() {
 	$(".show-options").click(function(){
 		$(".color-editor").slideDown(750);
 	});
-	
+	//widget size creator code
 	$(".wRow li").on("click", function(){
 		var column = $(this).data("column");
 		var row = $(this).data("row");
 		//console.log("column:" + column + ", row:" + row);
 		//remove class
-		$("li").removeClass("bar");
+		$("li").removeClass("highlight");
+		//only apply a class to a <ul> that is less than or equal to the row value of the <li> you clicked on
 		$(".wRow").filter(function(index){
-			if(index <= row){				
+			if(index <= row){
 				return true;
 			}
-			//console.log(row);				
-		})  .children()			
-			.addClass("bar");
+			//console.log(row);
+		})  .children()
+			.addClass("highlight");
+		//look through the <li>s and remove the class from any <li> that is in a column that is greate than or equal to the value of the <li> you clicked on
 		$(".wRow li").each(function(){
-			$(this).siblings()
-				   .eq(column)
-				   .prevAll()
-				   .removeClass("bar");
+		  $(this).siblings()
+		   .filter(function(index){
+		   		if(index >= column){
+		   			return true;
+		   		}
+		   	})
+		   .nextAll()
+		   .removeClass("highlight");
 		});
 	});
 //close jquery
