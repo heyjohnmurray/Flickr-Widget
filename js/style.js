4484dd4ea3c2dd98eef8904d9e9507c6feab6a17
John <jmurray@redventures.com>
2013-09-15 23:13:31 -0400

    working on reshaping image preview output

--- js/style.js
+++ js/style.js
@@ -40,7 +40,6 @@ $(function() {
 		});
 	});
 	//get flickr images and organize them on the page
-	var $children = $(".preview .images .flickr-image");
 	$.ajax({
 		dataType: "jsonp",		
 		url: "http://ycpi.api.flickr.com/services/feeds/photos_public.gne?id=27453474@N02&format=json&jsoncallback=?",
@@ -51,10 +50,11 @@ $(function() {
 			$(".loading").removeClass("show").addClass("hide");
 			for(var i=0; i<12; i++){
 				$(".preview .images").append("<div class='flickr-image'>" + "<img src='" + data.items[i].media.m + "' width='' height='' alt='' />" + "</div>");
-			}					
+			}
+			var $children = $(".preview .images .flickr-image");
 			for(var i = 0, l = $children.length; i < l; i += 4) {
 			    $children.slice(i, i+4).wrapAll('<div class="previewRow"></div>');
-			}			
+			}
 		},
 		error: function(){
 			console.log("something went wrong!");
@@ -62,11 +62,33 @@ $(function() {
 	});
 	//keep working on this
 	$(".js-get-code").on("click", function(){
-		var number = 2;
-		function restack(number){
-			for(var i = 0, l = $children.length; i < l; i += number) {
-			    $children.slice(i, i+number).wrapAll('<div class="previewRow"></div>');
+		var $children = $(".preview .images .flickr-image");
+		//removes current .previewRow class
+	    $children.unwrap();
+	    //reshape column output soon :: this doesn't work but i'm stopping here for now. 
+	    $.ajax({
+			dataType: "jsonp",		
+			url: "http://ycpi.api.flickr.com/services/feeds/photos_public.gne?id=27453474@N02&format=json&jsoncallback=?",
+			beforeSend: function(){
+				$(".loading").addClass("show");
+			},
+			success: function(data){
+				$(".loading").removeClass("show").addClass("hide");
+				for(var i=0; i<8; i++){
+					$(".preview .images").append("<div class='flickr-image'>" + "<img src='" + data.items[i].media.m + "' width='' height='' alt='' />" + "</div>");
+				}
+				var $children = $(".preview .images .flickr-image");
+				for(var i = 0, l = $children.length; i < l; i += 4) {
+				    $children.slice(i, i+4).wrapAll('<div class="previewRow"></div>');
+				}
+			},
+			error: function(){
+				console.log("something went wrong!");
 			}
+		});
+	    //reshapes row output : make "2" a var soon
+		for(var i = 0, l = $children.length; i < l; i += 2) {
+		    $children.slice(i, i+2).wrapAll('<div class="previewRow"></div>');
 		}
 	});
 //close jquery
